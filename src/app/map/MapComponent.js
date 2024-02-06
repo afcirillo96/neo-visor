2548b88d82e82b8327e505270c7ac4469946458d
afcirillo96 <agustincirillo@hotmail.com>
2024-02-06 12:33:25 -0300

    home control added

--- src/app/map/MapComponent.js
+++ src/app/map/MapComponent.js
@@ -2,18 +2,19 @@
 import React, { useEffect } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import MapLibreControlZoomHome from '../components/ui/maplibre-control-zoomhome'
 
 const MapComponent = () => {
   useEffect(() => {
     const map = new maplibregl.Map({
       container: 'map',
       style: '/style.json',
-      center: [-64.9395,-40.5736],
+      center: [-64.9395, -40.5736],
       zoom: 3.7,
       bearing: 0,
       pitch: 0,
     });
-    
+
     //Controls
     map.addControl(new maplibregl.NavigationControl(), 'top-right');
     map.addControl(new maplibregl.ScaleControl({}), 'bottom-right');
@@ -22,15 +23,28 @@ const MapComponent = () => {
     // map.addControl(new maplibregl.AttributionControl({compact: false}), 'bottom-right');
     // map.addControl(new maplibregl.MarkerControl({}), 'top-right');
 
+    const zoomhome = new MapLibreControlZoomHome({
+      // use a LngLatBounds or LngLatBoundsLike
+      resetBounds: [[-24.5525, 28.36838], [19.77906, 49.82957]],
+      /*
+      // use a LatLng or LatLngLike plus a zoom
+      resetLngLat: [100.66541, 16.15097],
+      resetZoom: 4,
+      */
+    });
+    map.addControl(zoomhome, 'top-right');
+
     return () => map.remove();
   }, []);
-  
+
   return (
-    React.createElement('div', { id: 'map', style: {
-      width: '100%',
-      height: '100vh'
-    }} )
+    React.createElement('div', {
+      id: 'map', style: {
+        width: '100%',
+        height: '100vh'
+      }
+    })
   );
 };
-  
+
 export default MapComponent;
