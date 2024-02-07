b6910fe5bba122f9beef7d49a61d5ff67d6ad309
afcirillo96 <agustincirillo@hotmail.com>
2024-02-07 12:49:46 -0300

    some draw tools added

--- src/app/map/MapComponent.js
+++ src/app/map/MapComponent.js
@@ -1,10 +1,15 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import MapLibreControlZoomHome from '../components/ui/maplibre-control-zoomhome';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+
 
 const MapComponent = () => {
+  const [drawnFeatures, setDrawnFeatures] = useState([]);
+
   useEffect(() => {
     const map = new maplibregl.Map({
       container: 'map',
@@ -19,8 +24,25 @@ const MapComponent = () => {
     //Controls
     const zoomhome = new MapLibreControlZoomHome({
       resetLngLat: [-64.9395, -40.5736],
-      resetZoom: 3.7,    
+      resetZoom: 3.7,
     });
+    MapboxDraw.constants.classes.CONTROL_BASE = 'maplibregl-ctrl';
+    MapboxDraw.constants.classes.CONTROL_PREFIX = 'maplibregl-ctrl-';
+    MapboxDraw.constants.classes.CONTROL_GROUP = 'maplibregl-ctrl-group';
+
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      controls: {
+        polygon: true,
+        line_string: true,
+        point: true,
+        circle: true,
+        rectangle: true,
+        trash: true,
+      },
+    });
+
+    map.addControl(draw, 'top-right');
     map.addControl(new maplibregl.NavigationControl(), 'top-right');
     map.addControl(zoomhome, 'top-right');
     map.addControl(new maplibregl.GeolocateControl({}), 'top-right');
@@ -29,6 +51,14 @@ const MapComponent = () => {
     // map.addControl(new maplibregl.AttributionControl(), 'bottom-left');
     // map.addControl(new maplibregl.AttributionControl({compact: false}), 'bottom-right');
     // map.addControl(new maplibregl.MarkerControl({}), 'top-right');
+
+    map.on('draw.create', ({ features }) => {
+      // Capturar datos del polígono dibujado
+      const drawnGeometry = features;
+      console.log(drawnGeometry)
+      // Almacenar datos del polígono
+      setDrawnFeatures([...drawnFeatures, drawnGeometry]);
+    });
 
     return () => map.remove();
   }, []);
