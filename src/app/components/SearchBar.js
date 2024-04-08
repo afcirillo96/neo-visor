10f65f03e9fe9299102609d424c729070b4ddcb9
afcirillo96 <agustincirillo@hotmail.com>
2024-04-08 10:03:12 -0300

    Settings.module.css renamed

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -1,11 +1,12 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-
+import styles from './Components.module.css';
+ 
 function SearchBar() {
   return (
     <div className="search-container">
       <input type="text" placeholder="Buscar..." />
-      <button type="submit">Buscar</button>
+      <button type="submit" className={styles.buttonS}>Buscar</button>
     </div>
   );
 }
--- src/app/components/Settings.js
+++ src/app/components/Settings.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import styles from './Settings.module.css';
+import styles from './Components.module.css';
 
 const Settings = () => {
     const [primaryColor, setPrimaryColor] = useState('#ffffff');
